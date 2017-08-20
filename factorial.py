a668960adf662c867ae8b15eb2fca7b87329d4ae
Nalaka1693 <nalaka1693@gmai.com>
2017-08-20 12:21:16 +0530

    first commit

---
+++ factorial.py
@@ -0,0 +1,36 @@
+fac_cache_r = {}
+fac_cache_i = {}
+
+
+def fac_r(n):
+  if n == 0:
+    return 1
+  elif n == 1:
+    return 1
+  else:
+    return n * fac_r(n - 1)
+
+
+def fac_i(n):
+  length = len(fac_cache_i)
+  fac_cache_i[0] = 1
+  if n == 0:
+    return 1
+  elif length - 1 >= n:
+    return fac_cache_i[n]
+  elif length - 1 < n:
+    f = fac_cache_i[length - 1]
+    for it in range(length, n + 1):
+      f *= it
+      fac_cache_i[it] = f
+    return f
+
+
+# for i in range(10):
+#   print fac_r(i)
+
+for i in range(7):
+  print fac_i(i)
+
+for i in range(4, 11):
+  print fac_i(i)
---
+++ fibonacci.py
@@ -0,0 +1,44 @@
+fib_r_cache_i = {}
+fib_r_cache_r = {}
+
+
+def fib_r(n):
+  if n == 1:
+    return 0
+  elif n == 2:
+    return 1
+  else:
+    return fib_r(n - 1) + fib_r(n - 2)
+
+
+def fib_i(n):
+  lenght = len(fib_r_cache_i)
+  fib_r_cache_i[0] = 0
+  fib_r_cache_i[1] = 1
+  if n == 1:
+    return 0
+  elif n == 2:
+    return 1
+  elif lenght >= n:
+    return fib_r_cache_i[n - 1]
+  else:
+    a = fib_r_cache_i[lenght - 2]
+    b = fib_r_cache_i[lenght - 1]
+    for it in range(lenght, n):
+      tmp = b
+      b = a + b
+      a = tmp
+      fib_r_cache_i[it] = b
+    return b
+
+
+# for i in range(1, 10):
+#   print fib_r(i)
+
+for i in range(1, 10):
+  print fib_i(i)
+
+print fib_r_cache_i
+
+for i in range(4, 12):
+  print fib_i(i)
